ee03dda492c1d03f7b01f9f086a045a3dc688702
anandlondhe <anandlondhe96@gmail.com>
2021-01-13 22:15:31 +0530

    Second Update
    Spaces Removed

--- registration-service/src/main/java/com/javatechie/reg/service/api/dao/UserRepository.java
+++ registration-service/src/main/java/com/javatechie/reg/service/api/dao/UserRepository.java
@@ -6,8 +6,6 @@ import com.javatechie.reg.service.api.model.User;
 
 import java.util.List;
 
-
-
 public interface UserRepository extends JpaRepository<User,Integer> {
     List<User> findByEmail(String email);
 }
